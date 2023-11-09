a49161d719babdae5f7da3fcb0319e800504621f
Wutdehel <elizanlilongongopan@gmail.com>
2023-11-09 22:18:03 +0700

    feat: get data from table

--- getdatasdb.sql
+++ getdatasdb.sql
@@ -5,4 +5,18 @@ SELECT * FROM users;
 
 SELECT * FROM barang;
 
-SELECT * 
+SELECT * FROM kategori_barang;
+
+SELECT * FROM penjualan_barang;
+
+-- @block
+
+-- select by id
+
+SELECT * FROM users WHERE id = 1;
+
+SELECT * FROM barang WHERE id = 1;
+
+SELECT * FROM kategori_barang WHERE id = 1;
+
+SELECT * FROM penjualan_barang WHERE id = 1;
