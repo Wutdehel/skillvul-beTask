70276bba276a0de08c890dd433ca937408525a3a
Wutdehel <elizanlilongongopan@gmail.com>
2023-11-09 22:24:04 +0700

    feat: edit data from table

---
+++ editdatadb.sql
@@ -0,0 +1,17 @@
+-- @block
+UPDATE users
+set username = "tatang seekor nyamuk" 
+where id = 2;
+
+UPDATE penjualan_barang
+SET total_pembelian = 3, harga_total = 45000000
+WHERE id = 1;
+
+update barang 
+set nama_barang = "victus gaming"
+WHERE id = 1;
+
+UPDATE kategori_barang
+set nama_kategori = "Laptop Dewa"
+WHERE id = 1
+
